a70f94f283c5acb5bb7bdcb3e0a74fdc29315c45
Pedro Luis Alcazar <alcazarjimenezpedro@gmail.com>
2025-01-22 20:57:25 -0600

    modificamos la variable numeros,para que la lista fuera aleatria

--- lista/filtrar_numeros.py
+++ lista/filtrar_numeros.py
@@ -1,8 +1,8 @@
-
+import random
 def filtrar_numero(numeros:list)->dict:
     diccionario_filtrar ={i:"par" if i % 2 == 0 else "impar" for i in numeros}
     return diccionario_filtrar
 
-numeros =[12,1,3,4,5,6,7,22,9,10]
+numeros = random.sample(range(1,20),10)
 salida = filtrar_numero(numeros)
 print(salida)            
