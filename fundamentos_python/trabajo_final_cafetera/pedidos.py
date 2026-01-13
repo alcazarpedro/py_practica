a644e1477d9fd0ff172f60383df69c4e2bb2cfc4
Pedro Luis Alcazar <alcazarjimenezpedro@gmail.com>
2026-01-12 20:49:50 -0600

    agregamos la estructura para crear el archivo pedidos , elegir las opciones de cafe

--- fundamentos_python/trabajo_final_cafetera/pedidos.py
+++ fundamentos_python/trabajo_final_cafetera/pedidos.py
@@ -1,11 +1,14 @@
+ARCHIVO_PEDIDOS = "pedidos.txt"
 def pedir_cafe():
+
     print("\nElige el cafe de tu agrado: ")
     print("1. Espresso")
     print("2. Latte")
     print("3. Capuccino")
     print("4. Americano")
 
-    opcion = input("opcion: ")
+    opcion = input("Elige una opcion que prefieras:  ")
+
 
     cafes = {
         "1" : "Espresso",
@@ -13,4 +16,18 @@ def pedir_cafe():
         "3" : "Capuccino",
         "4" : "Americano"
     }
+
+    if opcion  in cafes: # Valida contra las keys
+        cafe_elegido = cafes[opcion]
+        print(f"Has pedido un {cafe_elegido}.  estasmos preparando tu cafe")
+        #escritura modo append con utf-8
+        with open(ARCHIVO_PEDIDOS,"a",encoding="utf-8") as archivo:
+            archivo.write(cafe_elegido + "\n")
+    else:
+        print("Opcion no valida, Por favor intenta de nuevo")
+                
+
+
+
+
     
