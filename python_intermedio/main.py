1eb1d1a01006e05b8f46a97ba7249aacad6e309d
Pedro Luis Alcazar <alcazarjimenezpedro@gmail.com>
2026-01-13 20:51:10 -0600

    agregamos dos arcivos main.py y  compresiones.py

---
+++ python_intermedio/main.py
@@ -0,0 +1,17 @@
+"sistema  de analisis de noticas con APIS multiples"
+
+# Constantes de MAYUSCULAS  con guion bajo
+DEFAULT_LANGUAGE = "es"  # PEP8: comillas dobles para string
+
+
+# PEP-8: Utilidades comunes del proyecto - funciones snake_case
+def clean_text(text: str) -> str:
+    "limpia y formatea texto"
+    # implementacion posterior
+    pass
+
+
+def validate_api_key(api_key: str) -> bool:
+    """valida una API key para conectar servicios externos."""
+    # implemetacion posterior
+    pass
